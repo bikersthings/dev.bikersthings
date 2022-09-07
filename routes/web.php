6c8f51701a9e6bc312aab2a1227bcf0c27e3d06d
tebetebe19 <tubagusflow@gmail.com>
2022-09-07 21:56:21 +0700

    initial

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use App\Http\Controllers\Visitor\HomeController;
 // Route Visitor start
 
 Route::group(['prefix' => '/'], function(){
-    // Route::get('/', function () {
-    //     return view('page-visitor.home.main');
-    // });
+    Route::get('/', [HomeController::class,'index']);
 
     Route::get('/item-filter', function () {
         return view('page-visitor.item-filter.main');
@@ -42,86 +40,3 @@ Route::group(['prefix' => '/'], function(){
 // Route Visitor end
 
 
-
-
-
-
-
-
-
-// Route Panel User start
-
-Route::group(['prefix' => '/user'], function(){
-    
-    Route::get('/', function () {
-        return view('page-user.item.index');
-    });
-
-    Route::group(['prefix' => '/item'], function(){
-        Route::get('/', function () {
-            return view('page-user.item.index');
-        });
-        
-        Route::get('/create', function () {
-            return view('page-user.item.create');
-        });    
-    });
-
-    Route::get('/wishlist', function () {
-        return view('page-user.wishlist.index');
-    });
-
-    Route::get('/icebox', function () {
-        return view('page-user.icebox.index');
-    });
-
-    Route::get('/keranjang', function () {
-        return view('page-user.keranjang.index');
-    });
-
-    Route::get('/keuangan', function () {
-        return view('page-user.keuangan.index');
-    });
-
-    Route::get('/setting', function () {
-        return view('page-user.setting.index');
-    });
-
-});
-
-// Route Panel end
-
-
-
-
-
-
-
-
-
-// Route Panel Admin start
-
-Route::group(['prefix' => '/admin'], function(){
-
-    Route::get('/', function () {
-        return view('page-admin.item.index');
-    }); 
-    
-    Route::get('/item', function () {
-        return view('page-admin.item.index');
-    });  
-
-    Route::get('/icebox', function () {
-        return view('page-admin.icebox.index');
-    });  
-
-    Route::get('/user', function () {
-        return view('page-admin.user.index');
-    });  
-
-});
-
-// Route Panel Admin end
-
-
-Route::get('/', [HomeController::class,'index']);
